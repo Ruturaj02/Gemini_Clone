925103f79d9ed8560f16c229aca019fa38995acd
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-05-31 23:12:21 +0530

    Gemini Api key integrate with the project ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import { URL } from "./constants";
 
 function App() {
+  const [ask, setAsk] = useState("");
+  const [result, setResult] = useState("");
+
+const payLoad = {
+      "contents": [{
+        "parts": [{ "text": "Hi welcome" }]
+      }]
+    }
+
+  const askQuestion = async () => {
+    let response = await (URL,
+    {
+      method: "POST",
+      body: JSON.stringify(payLoad),
+    });
+    response = await response.JSON();
+    console.log(response.candidate[0].content.parts[0]);
+    setResult(response.candidate[0].content.parts[0].text);
+  };
   return (
     <>
       <div className="grid grid-cols-5 h-screen text-center bg-amber-300">
@@ -9,11 +30,17 @@ function App() {
         </div>
         <div className="col-span-4 p-10 ">
           <div className="container h-140">
-
+            {result}
           </div>
           <div className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-white flex h-16">
-            <input type="text" className="w-full h-full p-3 outline-none" placeholder="Ask Me Anything" />
-            <button>Ask</button>
+            <input
+              type="text"
+              value={ask}
+              onChange={(e) => setAsk(e.target.value)}
+              className="w-full h-full p-3 outline-none"
+              placeholder="Ask Me Anything"
+            />
+            <button onClick={() => askQuestion()}>Ask</button>
           </div>
         </div>
       </div>
